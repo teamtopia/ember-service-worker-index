a62b184da0e1f91e33e0f6838d6503bd0272d5c7
Chris Bonser <bonser.chris@gmail.com>
2021-09-30 14:43:40 -0500

    Rework the fallback strategy so that it works properly

--- service-worker/index.js
+++ service-worker/index.js
@@ -13,13 +13,9 @@ import cleanupCaches from 'ember-service-worker/service-worker/cleanup-caches';
 
 const CACHE_KEY_PREFIX = 'esw-index';
 const CACHE_NAME = `${CACHE_KEY_PREFIX}-${VERSION}`;
-
 const INDEX_HTML_URL = new URL(INDEX_HTML_PATH, self.location).toString();
 
-console.log(VERSION, 'what is self?', self);
-
 self.addEventListener('install', event => {
-  console.log(VERSION, 'self.addEventListener install', INDEX_HTML_URL);
   event.waitUntil(
     fetch(INDEX_HTML_URL, {credentials: 'include'}).then(response => {
       return caches
@@ -30,45 +26,24 @@ self.addEventListener('install', event => {
 });
 
 self.addEventListener('activate', event => {
-  console.log(VERSION, 'self.addEventListener activate');
   event.waitUntil(cleanupCaches(CACHE_KEY_PREFIX, CACHE_NAME));
 });
 
 self.addEventListener('fetch', event => {
-  console.log(VERSION, 'addEventListener fetch');
   let request = event.request;
   let url = new URL(request.url);
-  console.log(VERSION, 'addEventListener fetch - URL', url.href);
   let isGETRequest = request.method === 'GET';
   let acceptHeader =
     request.headers !== null ? request.headers.get('accept') : null;
-  console.log(VERSION, 'addEventListener fetch - accept header', acceptHeader);
   let isHTMLRequest =
     acceptHeader !== null ? acceptHeader.indexOf('text/html') !== -1 : true;
   let isLocal = url.origin === location.origin;
   let scopeExcluded = urlMatchesAnyPattern(request.url, INDEX_EXCLUDE_SCOPE);
-  console.log(VERSION, 'addEventListener fetch - scopeExcluded', scopeExcluded);
   let scopeIncluded =
     !INDEX_INCLUDE_SCOPE.length ||
     urlMatchesAnyPattern(request.url, INDEX_INCLUDE_SCOPE);
   let isTests = url.pathname === '/tests' && ENVIRONMENT === 'development';
 
-  console.log(
-    VERSION,
-    'addEventListener fetch conditions',
-    '!isTests',
-    !isTests,
-    'isGETRequest',
-    isGETRequest,
-    'isHTMLRequest',
-    isHTMLRequest,
-    'isLocal',
-    isLocal,
-    'scopeIncluded',
-    scopeIncluded,
-    '!scopeExcluded',
-    !scopeExcluded,
-  );
   if (
     !isTests &&
     isGETRequest &&
@@ -77,9 +52,8 @@ self.addEventListener('fetch', event => {
     scopeIncluded &&
     !scopeExcluded
   ) {
-    console.log(VERSION, 'addEventListener passed conditions');
     if (STRATEGY === 'fallback') {
-      cacheFallbackFetch(event, TIMEOUT);
+      event.respondWith(cacheFallbackFetch());
     } else {
       return cacheFirstFetch(event);
     }
@@ -87,17 +61,9 @@ self.addEventListener('fetch', event => {
 });
 
 function cacheFirstFetch(event) {
-  console.log(VERSION, 'cacheFirstFetch INDEX_HTML_URL', INDEX_HTML_URL);
-  console.log(VERSION, 'cacheFirstFetch CACHE_NAME', CACHE_NAME);
   return event.respondWith(
     caches.match(INDEX_HTML_URL, {cacheName: CACHE_NAME}).then(response => {
-      console.log(
-        VERSION,
-        'cacheFirstFetch -- looked up in cache and found:',
-        response,
-      );
       if (response) {
-        console.log(VERSION, 'cacheFirstFetch hit');
         return response;
       }
 
@@ -109,12 +75,6 @@ function cacheFirstFetch(event) {
       return fetch(INDEX_HTML_URL, {credentials: 'include'}).then(
         fetchedResponse => {
           caches.open(CACHE_NAME).then(cache => {
-            console.log(
-              VERSION,
-              '--------- cacheFirstFetch cache.put',
-              INDEX_HTML_URL,
-              fetchedResponse,
-            );
             return cache.put(INDEX_HTML_URL, fetchedResponse);
           });
           return fetchedResponse.clone();
@@ -124,42 +84,38 @@ function cacheFirstFetch(event) {
   );
 }
 
-function cacheFallbackFetch(event, fetchTimeout) {
-  try {
-    const FETCH_TIMEOUT = fetchTimeout;
-    console.log(VERSION, 'cacheFallbackFetch timeout', FETCH_TIMEOUT);
-    let didTimeOut = false;
-    new Promise(function (_resolve, reject) {
-      const timeout = setTimeout(function () {
-        didTimeOut = true;
-        reject(new Error('Request timed out'));
-      }, FETCH_TIMEOUT);
+function readFromCache() {
+  return caches.match(INDEX_HTML_URL, {cacheName: CACHE_NAME});
+}
 
-      return fetch(INDEX_HTML_URL, {credentials: 'include'})
-        .then(function (response) {
-          /**
-        Clear the timeout as cleanup
-      */
-          clearTimeout(timeout);
-          console.log(VERSION, 'cacheFallbackFetch in fetch.then', response);
-          if (!didTimeOut) {
-            console.log(VERSION, '-------- inserting into cache', CACHE_NAME);
-            caches
-              .open(CACHE_NAME)
-              .then(cache => cache.put(INDEX_HTML_URL, response));
-            return response.clone();
-          }
-        })
-        .catch(function (err) {
-          console.log(VERSION, 'cacheFallbackFetch first catch', e);
-          reject(err);
-        });
-    }).catch(function (err) {
-      console.log(VERSION, 'cacheFallbackFetch final catch');
-      return cacheFirstFetch(event);
-    });
+async function cacheFallbackFetch() {
+  // Race a timeout and fetch request
+  let timeoutPromise = new Promise(res =>
+    setTimeout(() => res('timeout'), TIMEOUT),
+  );
+  let fetchPromise = fetch(INDEX_HTML_URL, {credentials: 'include'});
+
+  try {
+    let result = await Promise.race([timeoutPromise, fetchPromise]);
+
+    // If the timeout won, fallback to the cache
+    if (result === 'timeout') {
+      return readFromCache();
+    }
+
+    // Clean up timeout
+    clearTimeout(timeoutPromise);
+
+    // Update the cache
+    let cache = await caches.open(CACHE_NAME);
+    cache.put(INDEX_HTML_URL, result.clone());
+
+    // Respond with result
+    return result;
   } catch (e) {
-    console.log(VERSION, 'cacheFallbackFetch error:', e);
-    throw e;
+    console.warn('cacheFallbackFetch -- failed for:', e);
+
+    // if the promise rejects, fallback to the cache
+    return readFromCache();
   }
 }
